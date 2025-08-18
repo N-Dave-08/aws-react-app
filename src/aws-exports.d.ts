a4540d9e9fe1e32519933a9b6d688d38404d8a8a
N-Dave-08 <cdave.dev@gmail.com>
2025-08-18 18:25:24 +0800

    feat: integrate aws amplify for authentication

---
+++ src/aws-exports.d.ts
@@ -0,0 +1,6 @@
+declare module "../aws-exports.js" {
+	const awsExports: unknown;
+	export default awsExports;
+}
+
+export {};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,35 @@
 import { Button } from "@/components/ui/button";
+import { Authenticator, withAuthenticator } from "@aws-amplify/ui-react";
 import { createFileRoute } from "@tanstack/react-router";
-
+import "@aws-amplify/ui-react/styles.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "../aws-exports.js";
+Amplify.configure(awsExports);
 // Export the component for testing
 export function App() {
 	return (
-		<div>
-			<Button>dasd</Button>
+		<div className="App">
+			<Authenticator>
+				{({ signOut }) => (
+					<main>
+						<header className="App-header">
+							{/* Sign Out Button */}
+							Welcome to the app
+							<Button
+								onClick={signOut}
+								style={{
+									margin: "20px",
+									fontSize: "0.8rem",
+									padding: "5px 10px",
+									marginTop: "20px",
+								}}
+							>
+								Sign Out
+							</Button>
+						</header>
+					</main>
+				)}
+			</Authenticator>
 		</div>
 	);
 }
@@ -13,3 +37,4 @@ export function App() {
 export const Route = createFileRoute("/")({
 	component: App,
 });
+export default withAuthenticator(App);
